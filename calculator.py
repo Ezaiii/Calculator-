e7583216dd9ef365e882059a42e0d3e20f2e02c7
Paramet Tom <tomparw@gmail.com>
2026-06-27 23:39:30 +0700

    add basicgui.py

---
+++ calculator.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from tkinter import ttk, messagebox
+
+GUI = Tk()
+GUI.title('Basic git & Github by Uncle engineer')
+GUI.geometry('500x700')
+
+L = Label(GUI,text='Loong Wissawakorn')
+L.pack()
+
+L = Label(GUI,text='Paramet Tom')
+L.pack()
+
+
+L= Label(GUI,text='s')
+L.pack()
+
+GUI.mainloop()
